bab6d0d08a0075bf1f7c3f90f1ce5c0c27fed1b4
huangweiliang <905387498@qq.com>
2017-05-31 09:17:55 +0800

    接口调试

--- js/index.js
+++ js/index.js
@@ -1,8 +1,47 @@
-window.onload = function(){
-  mui.init(); 
+mui.ready(function() {
+  mui.init();
 
-  document.querySelector(".J_fixedTopClrear").onclick = function(){
+  document.querySelector(".J_fixedTopClrear").onclick = function () {
     document.querySelector(".J_fixedTop").style.display = "none";
   }
 
-}
+  //初始化数据
+  mui.post(getUrl(), {
+    username: '111',
+    password: 'FFFF'
+  }, function (data) {
+      var goodsList = data.data.promotion;
+      for(var i=0;i<goodsList.length; i++) {
+        var str = '<li>';
+            str += '<a href="' + goodsList[i].goods_url + '?id=' + goodsList[i].id + '">';
+            str += '<img src="images/index/goods001.jpg" alt="">';
+            str += '<h3>' + goodsList[i].name + '</h3>';
+            str += '<p>';
+            str += '<span>￥' + goodsList[i].price + '</span>';
+            str += '<button class="fr">订购</button>';
+            str += '</p>';
+            str += '</a>';
+            str += '</li>';
+        $('.J_goodsList').append($(str));
+      }
+      var list = data.data.normal_goods;
+      for(var i=0;i<list.length; i++) {
+        var str = '<li>';
+            str += '<a href="' + list[i].goods_url + '?id=' + list[i].id + '">';
+            str += '<div class="fl list_L">';
+            str += '	<img src="images/index/goods005.png" alt="">';
+            str += '</div>';
+            str += '<div class="fl list_R">';
+            str += '<h3>' + list[i].name + '</h3>';
+            str += '<p>￥' + list[i].price + '</p>';
+            str += '<button>订购</button>';
+            str += '</div>';
+            str += '</a>';
+            str += '</li>';
+        $('.J_list').append($(str));
+      }
+
+    }, 'json'
+  );
+
+})   
---
+++ js/utils.js
@@ -0,0 +1,5 @@
+function getUrl() {
+  // return 'http://172.19.5.104/saver/health_mui/index.php';
+  return 'http://172.19.5.104/saver/health_mui/middleware.php';
+  // return 'http://demo.lmqde.com/api/goods/home_goods';
+}
